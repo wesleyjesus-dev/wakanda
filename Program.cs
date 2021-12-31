b30f015d3f94498ed9fcccf56b7a6310e6ae7cac
Wesley Jesus <wesleydsjesus@gmail.com>
2021-12-31 02:58:55 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using k8s;
+
+var config = KubernetesClientConfiguration.BuildDefaultConfig();
+IKubernetes client = new Kubernetes(config);
+
+var currentNamespace = config.Namespace;
+
+var deployments = client.ListNamespacedDeployment(currentNamespace);
+
+Console.Write("######## Deployment ########\n");
+foreach (var deployment in deployments.Items)
+{
+    Console.Write($"Name: {deployment.Metadata.Name}\n");
+    Console.Write($"Available Replicas: {deployment.Status.AvailableReplicas}\n");
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+## Wakanda
+
+___
+
+The sample cli project to kubernetes in C#
+
+---
